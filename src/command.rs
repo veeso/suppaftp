3f035b95e233ef2e678acd3c8fd8233a1aa1a231
veeso <christian.visintin1997@gmail.com>
2021-12-07 15:43:17 +0100

    lint

--- src/command.rs
+++ src/command.rs
@@ -12,8 +12,10 @@ pub enum Command {
     /// Append to file
     Appe(String),
     /// Set auth to TLS
+    #[cfg(any(feature = "secure", feature = "async-secure"))]
     Auth,
     /// Ask server not to encrypt command channel
+    #[cfg(any(feature = "secure", feature = "async-secure"))]
     ClearCommandChannel,
     /// Change directory to parent directory
     Cdup,
@@ -36,6 +38,7 @@ pub enum Command {
     /// Passive mode
     Pasv,
     /// Protection buffer size
+    #[cfg(any(feature = "secure", feature = "async-secure"))]
     Pbsz(usize),
     /// Specifies an address and port to which the server should connect (active mode)
     Port(String),
@@ -79,8 +82,10 @@ impl ToString for Command {
     fn to_string(&self) -> String {
         let mut s = match self {
             Self::Appe(f) => format!("APPE {}", f),
+            #[cfg(any(feature = "secure", feature = "async-secure"))]
             Self::Auth => "AUTH TLS".to_string(),
             Self::Cdup => "CDUP".to_string(),
+            #[cfg(any(feature = "secure", feature = "async-secure"))]
             Self::ClearCommandChannel => "CCC".to_string(),
             Self::Cwd(d) => format!("CWD {}", d),
             Self::Dele(f) => format!("DELE {}", f),
@@ -97,6 +102,7 @@ impl ToString for Command {
             Self::Noop => "NOOP".to_string(),
             Self::Pass(p) => format!("PASS {}", p),
             Self::Pasv => "PASV".to_string(),
+            #[cfg(any(feature = "secure", feature = "async-secure"))]
             Self::Pbsz(sz) => format!("PBSZ {}", sz),
             Self::Port(p) => format!("PORT {}", p),
             #[cfg(any(feature = "secure", feature = "async-secure"))]
@@ -143,7 +149,9 @@ mod test {
                 .as_str(),
             "APPE foobar.txt\r\n"
         );
+        #[cfg(any(feature = "secure", feature = "async-secure"))]
         assert_eq!(Command::Auth.to_string().as_str(), "AUTH TLS\r\n");
+        #[cfg(any(feature = "secure", feature = "async-secure"))]
         assert_eq!(Command::ClearCommandChannel.to_string().as_str(), "CCC\r\n");
         assert_eq!(Command::Cdup.to_string().as_str(), "CDUP\r\n");
         assert_eq!(
@@ -184,6 +192,7 @@ mod test {
             "PASS qwerty123\r\n"
         );
         assert_eq!(Command::Pasv.to_string().as_str(), "PASV\r\n");
+        #[cfg(any(feature = "secure", feature = "async-secure"))]
         assert_eq!(Command::Pbsz(0).to_string().as_str(), "PBSZ 0\r\n");
         assert_eq!(
             Command::Port(String::from("0.0.0.0:21"))
